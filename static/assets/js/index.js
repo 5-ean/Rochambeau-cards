7c6b706bc56c41d928ba3825eedfa334c4945f5b
5-ean <seandowden@hotmail.co.uk>
2021-07-08 01:19:19 +0000

    Added how to play instructions feature

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,4 +1,5 @@
-// Colour picker JavaScript
+
+// Colour Picker JavaScript
 let cup1 = document.getElementById("cup1");
 let cup2 = document.getElementById("cup2");
 let cup3 = document.getElementById("cup3");
@@ -10,3 +11,21 @@ colorpicker.on('color:change', function(color) {
     cup2.style.borderBottomColor = color.hexString;
     cup3.style.borderBottomColor = color.hexString;
 });
+
+// How To Play Instructions Displayer
+var instructions = document.getElementById("instructions");
+var intructionbtn = document.getElementById("intructionbtn");
+
+intructionbtn.onclick = function(){
+
+    if(instructions.className == "close") {
+        //open the instructions
+        instructions.className = ""
+        intructionbtn.innerHTML = "I Got It!";
+    } else {
+        // close the instructions
+        instructions.className = "close";
+        intructionbtn.innerHTML = "How To Play";
+    }
+
+};
